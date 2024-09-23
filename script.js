14b3a35e4e26906711dd1c46d55535c04fd2772c
BRO-BHATT22 <144465378+BRO-BHATT22@users.noreply.github.com>
2024-09-23 09:17:33 +0000

    added java and more prog

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 function stud() {
     document.getElementById('swapImage').src = 'tunnel.jpg'
     document.getElementById('response-text').innerText = 'The Lincoln Tunnel, an engineering marvel connecting Weehawken, New Jersey, to Midtown Manhattan in New York City, opened its portals to the public on December 22, 1937. With great anticipation and ceremony, this groundbreaking transportation link beneath the Hudson River marked a significant milestone in urban development and interstate connectivity.  '
-    document.getElementById('response-date').innerText = 'December 22, 1937'
+    document.getElementById('response-date').innerText = 'Student Life'
 }
 
 function finan() {
     document.getElementById('swapImage').src = 'dababy.webp'
     document.getElementById('response-text').innerText = 'DaBaby, the acclaimed American rapper and songwriter, was born on December 22, 1991, in Cleveland, Ohio. Born Jonathan Lyndale Kirk, his early life set the stage for a remarkable journey into the music industry. From humble beginnings, DaBabys birth marked the beginning of a future stars life. '
-    document.getElementById('response-date').innerText = 'December 22, 1991'
+    document.getElementById('response-date').innerText = 'Cost and Financial Aid'
 }
 function perc() {
     document.getElementById('swapImage').src = 'tiktok.png'
     document.getElementById('response-text').innerText = 'TikTok users often reference something they call the December 22 incident, which often leaves viewers confused as to what it means. The December 22 incident, or the TikTok slideshow incident, refers to the date when TikTok slideshows sharing adult content hidden in the slides were posted without moderators taking them down. '
-    document.getElementById('response-date').innerText = 'December 22, 2022'
+    document.getElementById('response-date').innerText = 'Fun Percentage'
 }
 function acad() {
     document.getElementById('swapImage').src = 'dreyfus.webp'
     document.getElementById('response-text').innerText = 'Alfred Dreyfus, a French military officer, was sentenced on December 22, 1894, in a trial that would later become a symbol of injustice and anti-Semitism. Accused of passing military secrets to Germany, Dreyfus faced a court-martial that was marred by irregularities, including the suppression of exculpatory evidence. Despite a lack of conclusive proof, he was found guilty, stripped of his rank, and publicly degraded in a ceremony known as "degradation." '
-    document.getElementById('response-date').innerText = 'December 22, 1894'
+    document.getElementById('response-date').innerText = 'Academic Programs'
 }
 function alum() {
     document.getElementById('swapImage').src = 'dadt.jpg'
     document.getElementById('response-text').innerText = 'On December 22, 2010, a historic moment occurred in the United States military as the "Dont Ask, Dont Tell" (DADT) policy was officially repealed. Enacted in 1993, DADT prohibited openly gay, lesbian, and bisexual individuals from serving in the U.S. military. The repeal marked a significant stride towards equal rights and inclusivity, allowing LGBTQ+ service members to serve openly without fear of discrimination or discharge based on their sexual orientation'
-    document.getElementById('response-date').innerText = 'December 22, 2010'
+    document.getElementById('response-date').innerText = 'Alumni Success'
 }
 
-function geninfor() {
+function geninfo() {
     document.getElementById('swapImage').src = 'Michigan.jpg'
     document.getElementById('response-text').innerText = 'The University of Michigan, established in 1817 in Ann Arbor, is a leading public research university. It offers a wide range of top-ranked programs, particularly in engineering, business, law, and medicine. Known for its vibrant campus life, diverse student body, and strong research focus, U-M fosters innovation and public service. Its alumni network is influential worldwide, and the university consistently ranks among the best in the U.S.'
     document.getElementById('response-date').innerText = 'General Info'
